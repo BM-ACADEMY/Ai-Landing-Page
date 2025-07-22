46865797f51255065aa3c891518566bc53ade099
BM-ACADEMY <teamkamarweb@gmail.com>
2025-07-22 18:01:25 +0530

    payment

--- client/src/Module/PricingSlider.jsx
+++ client/src/Module/PricingSlider.jsx
@@ -35,7 +35,7 @@ const PricingSlider = () => {
         "Language: Tamil & English",
         "Certificate of Completion",
       ],
-      upiAmount: "999",
+      upiAmount: "1",
       popular: false,
     },
     {
